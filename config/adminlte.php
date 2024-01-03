30464a094e1d0e7b84bd715aadb18bbb1867e50d
Mauro <maurogt.dd@gmail.com>
2024-01-02 22:55:17 -0300

    revierto mostrar Registro

--- config/adminlte.php
+++ config/adminlte.php
@@ -255,7 +255,7 @@ return [
     'dashboard_url' => 'inicio',
     'logout_url' => 'logout',
     'login_url' => 'login',
-    'register_url' => 'register',
+    'register_url' => false,
     'password_reset_url' => 'password/reset',
     'password_email_url' => 'password/email',
     'profile_url' => false,
