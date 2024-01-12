4c29ef6b6d354bcc06109c57ccbe04e7fa0aca7a
Mauro <maurogt.dd@gmail.com>
2024-01-11 21:12:06 -0300

    test manifest https

--- resources/views/vendor/laravelpwa/meta.blade.php
+++ resources/views/vendor/laravelpwa/meta.blade.php
@@ -1,5 +1,5 @@
 <!-- Web Application Manifest -->
-<link rel="manifest" href="{{ route('laravelpwa.manifest') }}">
+<link rel="manifest" href="{{ URL::secure(route('laravel.manifest')) }}">
 <!-- Chrome for Android theme color -->
 <meta name="theme-color" content="{{ $config['theme_color'] }}">
 
