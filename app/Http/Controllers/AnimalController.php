bb615e12f8ea764e5775eab616be59098334231a
= <=>
2024-02-22 21:26:24 -0300

    test

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -92,7 +92,8 @@ class AnimalController extends Controller
             $fileName = time() . '_' . $file->getClientOriginalName();
             $filePath = $file->storeAs('uploads', $fileName); // Almacenar el archivo en storage/uploads
         
-            Excel::import(new AnimalsImport, storage_path('app/' . $filePath), 'local', \Maatwebsite\Excel\Excel::XLSX);
+            Excel::import(new AnimalsImport, storage_path('app/' . $filePath));
+            // , 'local', \Maatwebsite\Excel\Excel::XLSX);
 
         }
 
